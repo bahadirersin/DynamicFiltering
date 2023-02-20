ee37a525fff2869ec532c1a87471e549bac6caee
bahadirersin <bahadirersin@gmail.com>
2023-02-20 15:28:43 +0300

    Initial Commit

---
+++ DynamicFiltering/DynamicFilteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicFilteringApp.swift
+//  DynamicFiltering
+//
+//  Created by Bahadır Ersin on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicFilteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
